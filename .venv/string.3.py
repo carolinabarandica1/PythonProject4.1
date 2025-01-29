56e907b35d886b008add155ccac99a188766ce49
carolinabarandica <carolinabarandica1@gmail.com>
2025-01-29 17:18:53 +0100

    1st commit

--- .venv/string.3.py
+++ .venv/string.3.py
@@ -5,3 +5,5 @@ print(s[1:7])
 print(s[:7])# if you omit the first index it starts from beginning
 s="i go to school early in the morning"
 print(s[:20])
+print(s[:]) # the whole thing
+print(s[::2])# for every second character
