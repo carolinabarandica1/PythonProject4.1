04539409f7f8a8a5c2073746a424ae32e619fa4f
carolinabarandica <carolinabarandica1@gmail.com>
2025-01-29 18:09:07 +0100

    1st commit

--- .venv/string 4.py
+++ .venv/string 4.py
@@ -3,4 +3,9 @@ print(dir(s))
 useful_methods = [m for m in dir(s) if "__" not in  m]
 print(useful_methods)
 print(help(s.capitalize))
-print(s.capitalize)
+print(s.capitalize())
+print("HELLO".casefold())
+
+print("hello".center(50))
+print("i see a little dove".count("e"))
+
