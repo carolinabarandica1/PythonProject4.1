7accc25df6fb0c721ad26988a195e8780186e22f
carolinabarandica <carolinabarandica1@gmail.com>
2025-01-29 16:48:49 +0100

    1st commit

---
+++ .venv/session # 6.py
@@ -0,0 +1,25 @@
+sum = 0
+for num in range(1,101):
+    sum= sum+num
+print(sum)
+
+for i in range (1,11):
+    for j in range (1,11):
+        print(f"{i} X {j} = {i+j}")
+
+    # rewrite it using while loop
+sum = 0
+num = 0
+while num <100:
+    num = num + 1
+    sum += num
+print(sum)
+
+print()  # to separate with a new line between blocks
+
+i = 20
+print("i i i")
+print(f"{i}{21 + i} i i")
+
+
+
